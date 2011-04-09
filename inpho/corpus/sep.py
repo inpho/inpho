181721e12c63016ff544339b74938b22f60f5fc1
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-04-08 23:30:03 -0400

    adding corpus module

---
+++ inpho/corpus/sep.py
@@ -0,0 +1,175 @@
+import os
+import os.path
+from BeautifulSoup import BeautifulSoup
+from sqlalchemy.orm import subqueryload
+from sqlalchemy import and_, or_, not_
+
+import inpho.corpus.stats as dm
+from inpho.model import Idea, Thinker, Entity, Session 
+
+def extract_article_body(filename):
+    f=open(filename)
+    doc=f.read()
+    soup=BeautifulSoup(doc)
+
+    # rip out bibliography
+    biblio_root = soup.findAll('h2', text='Bibliography')
+    if biblio_root:
+        biblio_root = biblio_root[-1].findParent('h2')
+        biblio = [biblio_root]
+        biblio.extend(biblio_root.findNextSiblings())
+        biblio = [elm.extract() for elm in biblio]
+
+    # grab modified body 
+    body=soup.find("div", id="aueditable")
+
+    return body.text
+
+def select_terms(entity_type=Idea):
+    # process entities
+    ideas = Session.query(entity_type)
+    ideas = ideas.options(subqueryload('_spatterns'))
+    # do not process Nodes or Journals
+    ideas = ideas.filter(and_(Entity.typeID!=2, Entity.typeID!=4))
+    return ideas.all()
+     
+
+def process_article(article, terms=None, entity_type=Idea, output_filename=None,
+                    corpus_root='corpus/'):
+    if terms is None:
+        terms = select_terms(entity_type)
+    
+
+    lines = []
+
+    filename = article.get_filename(corpus_root)
+    if filename and os.path.isfile(filename):
+        print "processing:", article.sep_dir, filename
+        try: 
+            doc = extract_article_body(filename)
+            lines = dm.prepare_apriori_input(doc, terms, article)
+        except:
+            print "ERROR PROCESSING:", article.sep_dir, filename
+    else:
+        print "BAD SEP_DIR:", article.sep_dir
+
+    if output_filename:
+        with open(output_filename, 'w') as f:
+            f.writelines(lines)
+    else:
+        return lines
+
+from multiprocessing import Pool
+
+def process_wrapper(args):
+    return process_article(*args)
+
+def process_articles(entity_type=Entity, output_filename='output-all.txt',
+                     corpus_root='corpus/'):
+    terms = select_terms(entity_type)
+    Session.expunge_all()
+    
+    articles = Session.query(entity_type).filter(entity_type.sep_dir!='').all()
+   
+    # parallel processing of articles
+    p = Pool()
+    args = [(title, terms, entity_type) for title in articles]
+    doc_lines = p.map(process_wrapper, args)
+    p.close()
+
+    # write graph output to file
+    with open(output_filename, 'w') as f:
+        for lines in doc_lines:
+            f.writelines(lines)
+
+import subprocess
+
+def complete_mining(entity_type=Idea, filename='graph.txt', root='./'):
+    occur_filename = os.path.abspath(root + "graph-" + filename)
+    edge_filename = os.path.abspath(root + "edge-" + filename)
+    sql_filename = os.path.abspath(root + "sql-" + filename)
+
+
+    print "processing articles..."
+    process_articles(entity_type, occur_filename)
+
+    print "running apriori miner..."
+    dm.apriori(occur_filename, edge_filename)
+    
+    print "processing edges..."
+    edges = dm.process_edges(occur_filename, edge_filename)
+    ents = dm.calculate_node_entropy(edges)
+    edges = dm.calculate_edge_weight(edges, ents)
+    
+    print "creating sql files..."
+
+    with open(sql_filename, 'w') as f:
+        for edge, props in edges.iteritems():
+            ante,cons = edge
+            row = "%s::%s" % edge
+            row += "::%(confidence)s::%(jweight)s::%(weight)s\n" % props
+            f.write(row)
+
+    print "updating term entropy..."
+
+    for term_id, entropy in ents.iteritems():
+        term = Session.query(Idea).get(term_id)
+        if term:
+            term.entropy = entropy
+
+    Session.flush()
+    Session.commit()
+
+
+    # Import SQL statements
+    if entity_type == Idea:
+        table = "idea_graph_edges"
+    elif entity_type == Thinker:
+        table = "thinker_graph_edges"
+    else:
+        table = "idea_thinker_graph_edges"
+
+    connection = Session.connection()
+
+    print "deleting old graph information ..."
+    connection.execute("""
+    DELETE FROM %(table)s;
+    """ % {'filename' : sql_filename, 'table' : table })
+    
+    print "inserting new graph information"
+    connection.execute("""
+    
+    LOAD DATA INFILE '%(filename)s'
+    INTO TABLE %(table)s
+    FIELDS TERMINATED BY '::'
+    (ante_id, cons_id, confidence, jweight, weight);
+    
+    """ % {'filename' : sql_filename, 'table' : table })
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+
+    usage = "usage: %prog [options] config_file"
+    parser = OptionParser(usage)
+    parser.set_defaults(mode='all')
+    parser.add_option("-a", "--all", action="store_const",
+                      dest='mode', const='all',
+                      help="mine all edges [default]")
+    parser.add_option("-i", "--idea", action="store_const",
+                      dest='mode', const='idea',
+                      help="mine only idea-idea edges")
+    parser.add_option("-t", "--thinker", action="store_const",
+                      dest='mode', const='thinker',
+                      help="mine only thinker-thinker edges")
+    options, args = parser.parse_args()
+
+    if options.mode == 'all':
+        complete_mining(Entity)
+
+    elif options.mode == 'idea':
+        complete_mining(Idea)
+
+    elif options.mode == 'thinker':
+        complete_mining(Thinker)
+
---
+++ inpho/corpus/stats.py
@@ -0,0 +1,159 @@
+#loadCorpus
+
+# Iterate through corpus dir and find all sep_dirs and index files
+# Alternatively, iterate through sep_dirs of SQL
+
+# Open HTML file and grab sentence list
+# 
+# title.replaceAll(" \\(stanford encyclopedia of philosophy\\)", "");                 
+# title.replaceAll("sep: ", "");   
+
+# documentString which is just sentenceList.join()
+
+# appearList is set of all terms which appear in the sentence
+# occurList is set of all terms occuring in the document
+
+import re
+import string
+import subprocess
+from collections import defaultdict
+
+# http://nltk.googlecode.com/svn/trunk/doc/api/nltk.tokenize.punkt.PunktSentenceTokenizer-class.html
+from nltk.tokenize import PunktSentenceTokenizer as Tokenizer
+
+def get_document_occurrences(document, terms, doc_term=None):
+    occurrences = set()
+    
+    # iterate over terms to be scanned
+    for term in terms:
+        if term != doc_term:
+            pattern = ''
+            try:
+                if re.search('\b%s\b' % term.label, document,
+                                flags=re.IGNORECASE):
+                    occurrences.add(term)
+                else:
+                    for pattern in term.searchpatterns:
+                        if re.search(pattern, document, flags=re.IGNORECASE):
+                            occurrences.add(term)
+                            break
+            except:
+                print "ERROR HANDLING:", term.ID, term.label, pattern
+
+    occurrences = list(occurrences)
+
+    if doc_term:
+        occurrences.append(doc_term)
+
+    return occurrences
+
+def get_sentence_occurrences(document, terms, doc_term=None):
+    terms_present = get_document_occurrences(document, terms)
+
+    # Use a Tokenizer from NLTK to build a sentence list
+    tokenizer = Tokenizer(document)
+    sentences = tokenizer.tokenize(document)
+    
+    # Create a list of lists containing the collection of terms which cooccurr
+    # in a sentence
+    occurrences = []
+    for sentence in sentences:
+        sentence_occurrences = set() 
+
+        for term in terms_present:
+            if term != doc_term:
+                if re.search(' %s ' % term.label, sentence):
+                    sentence_occurrences.add(term)
+        
+
+        if len(sentence_occurrences) > 0:
+            sentence_occurrences = list(sentence_occurrences)
+            to_remove = set()
+
+            for inside in sentence_occurrences:
+                for term in sentence_occurrences:
+                    if term != inside and\
+                        term.label.find(inside.label) != -1:
+                        to_remove.add(inside)
+            
+            if to_remove:
+                print "removing", to_remove
+
+            for term in to_remove:
+                sentence_occurrences.remove(term)
+
+            if doc_term:
+                sentence_occurrences.append(doc_term)
+
+            occurrences.append(sentence_occurrences)
+    
+    return occurrences
+
+def prepare_apriori_input(document, terms, doc_term=None, add_newline=True): 
+    occurrences = get_document_occurrences(document, terms, doc_term)
+    sentence_occurrences = get_sentence_occurrences(document, terms, doc_term)
+
+    lines = []
+    lines.append(string.join([str(term.ID) for term in occurrences]))
+    for sent_occur in sentence_occurrences:
+        lines.append(string.join([str(term.ID) for term in sent_occur]))
+
+    if add_newline:
+        lines = [line + '\n' for line in lines]
+
+    return lines
+
+def apriori(input_filename='output.txt', output_filename='edges.txt'):
+    args = ['apriori', input_filename, output_filename,
+            '0.00000000000000001', '0.00000000000000001']
+    return subprocess.call(args)
+
+
+def process_edges(occurrences_filename='output.txt', edges_filename='edges.txt'):
+    # process occurrence and cooccurrence data
+    occurrences = defaultdict(lambda: defaultdict(int))
+    with open(occurrences_filename) as f:
+        for line in f:
+            ids = line.split()
+
+            for ante in ids:
+                for cons in ids:
+                    occurrences[ante][cons] += 1
+
+    edges = defaultdict(dict)
+    with open(edges_filename) as f:
+        for line in f:
+            ante,cons,confidence,jweight = line.split()
+            edges[(ante,cons)] = {'confidence':float(confidence), 
+                                  'jweight':float(jweight),
+                                  'occurrences':occurrences[ante][cons]}
+    return edges
+
+from math import log
+def calculate_node_entropy(edges):
+    probabilities = defaultdict(lambda: defaultdict(float))
+    for edge,properties in edges.iteritems():
+        ante,cons = edge
+        probabilities[ante][cons] = properties['confidence']
+    
+    ents = defaultdict(float)
+    for ante,conses in probabilities.iteritems():
+        total_prob = sum(conses.values())
+        normalized_probs = [x / total_prob for x in conses.values()]
+
+        Hx = sum([x * (log(x) / log(2)) for x in normalized_probs])
+
+        ents[ante] = -Hx
+
+    return ents
+
+def calculate_edge_weight(edges, ents):
+    weights = defaultdict(float)
+    max_entropy = max(ents.values())
+    for edge, properties in edges.iteritems():
+        ante,cons = edge
+        entropy_diff = ents[ante] - ents[cons]
+        entropy_norm = entropy_diff / max_entropy
+        properties['weight'] = properties['jweight'] * entropy_norm
+
+    return edges    
