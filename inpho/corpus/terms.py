4823541119adca88c88a2080a47eb55e7cc8e3c4
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-05-21 16:29:55 -0400

    added graph term loading and optimized Term

--- inpho/corpus/terms.py
+++ inpho/corpus/terms.py
@@ -1,13 +1,29 @@
 import os.path
+import pickle
+
 from sqlalchemy.orm import subqueryload
 from sqlalchemy import and_, or_, not_
 
 from inpho.model import Idea, Thinker, Entity, Session 
 
 class Term(object):
-    def __init__(self, label, searchpatterns=[]):
+    __slots__ = ['label', 'searchpatterns', 'source', 'ID']
+        
+    def __init__(self, label, searchpatterns=[], source=None, ID=None):
         self.label = label
         self.searchpatterns = searchpatterns
+        
+        self.source = source
+        self.ID = ID
+
+    def __repr__(self):
+        if self.source and self.ID is not None:
+            return '<Term %s-%d: %s>' % (self.source, self.ID, self.label.encode('utf-8'))
+        else:
+            return '<Term: %s>' % (self.source, self.label.encode('utf-8'))
+
+    def __str__(self):
+        return self.label.encode('utf-8')
 
 def inpho_terms(entity_type=Idea):
     # process entities
@@ -16,8 +32,18 @@ def inpho_terms(entity_type=Idea):
     # do not process Nodes or Journals
     ideas = ideas.filter(and_(Entity.typeID!=2, Entity.typeID!=4))
     ideas = ideas.all()
-    
+
     Session.expunge_all()
     Session.close()
+
+    ideas = [Term(idea.label, idea.searchpatterns, 'InPhO', idea.ID)
+                 for idea in ideas]
     return ideas
 
+def graph_terms(filename, source_name=None):
+    with open(filename, 'r') as f:
+        graph = pickle.load(f)
+
+    terms = [Term(data['label'], source=source_name)
+                 for id, data in graph.nodes_iter(data=True)]
+    return terms
