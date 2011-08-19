17565c3f1aa8eefca43db13da709d58180eed123
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-08-19 17:59:09 -0400

    adding documentation

--- inpho/taxonomy/__init__.py
+++ inpho/taxonomy/__init__.py
@@ -1,14 +1,10 @@
 from collections import defaultdict
 import re
 
-""" 
-A taxonomy is a directed graph consisting of *instances* and *links*.
-
-The optional "spine" specifies a collection of hand-coded instances, which
-may be ommitted from the usual taxonomy evaluation techniques.
-"""
-
 class Node(object):
+    """
+    Class to represent an InPhO taxonomy
+    """
     def __init__(self, value=None, spine=None):
         self.value = value
         self.spine = spine
@@ -32,29 +28,41 @@ class Node(object):
     
     @property
     def root(self):
+        """ Returns the root parent node. """
         return self.parent.root if self.parent else self
 
     def graft(self, child):
+        """ Appends the Node as a child. """
         self._children.add(child)
         child.parent = self
         return self
 
     def prune(self, child):
+        """ 
+        Removes the given child Node. 
+        Raises KeyError if child is not found in children.
+        """
         self._children.remove(child)
         child.parent = None
         return child
 
     def fragment(self):
-        self.parent._children.remove(self)
-        self.parent = None
+        """ 
+        Removes this Node from its parent and returning a free-standing tree.
+        """
+        if self.parent is not None:
+            self.parent._children.remove(self)
+            self.parent = None
         return self
 
     def siblings(self):
+        """ Returns the list of siblings """
         siblings = list(self.parent.children)
         siblings.remove(self)
         return siblings
 
     def search(self, needle):
+        """ Performs a depth-first search for the given needle. """
         if self.value == needle:
             return self
         
@@ -67,33 +75,42 @@ class Node(object):
 
 def from_dlv(filename):
     """
-    Function to return a taxonomy from the specified DLV output file.
+    Function to build a taxonomy from the specified DLV output file.
     """
     # build regex for instance and link search
     regex_class = re.compile("class\(i(\d+)\)")
     regex_ins = re.compile("[ins|isa]\(i(\d+),i(\d+)\)")
     regex_links = re.compile("link\(i(\d+),i(\d+)\)")
- 
+
+    # process DLV output file
     with open(filename) as f:
         dlv = f.read()
 
-        classes = frozenset(regex_class.findall(dlv))
-        instances = frozenset(regex_ins.findall(dlv))
-        links = frozenset(regex_links.findall(dlv))
+        classes = regex_class.findall(dlv)
+        instances = regex_ins.findall(dlv)
+        links = regex_links.findall(dlv)
 
+    # set up taxonomy structure
     nodes = defaultdict(Node)
     root = Node("Philosophy", spine=True)
 
+    # populate instances
     for child, parent in instances:
         nodes[parent].graft(nodes[child])
 
+    # populate links
     for target, source in links:
         nodes[source].links.add(nodes[target])
 
+    # glue taxonomies together, initialize values
     for key,node in nodes.iteritems():
         node.value = key
+
+        # specify hand-built portion of the taxonomy
         if node.value in classes:
             node.spine = True
+
+        # if this is a root, glue it to the Philosophy node.
         if node.parent is None:
             root.graft(node)
 
