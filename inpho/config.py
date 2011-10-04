51a6d83a82e2a99aec89e47ccae3ca19481f0275
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-10-04 00:24:18 -0400

    added logging to config file

--- inpho/config.py
+++ inpho/config.py
@@ -2,6 +2,7 @@
 Imports configuration files for use with the inpho libraries.
 '''
 import os.path
+import logging
 config_paths = [os.path.abspath("inpho.ini"), 
                 os.path.expanduser("~/.config/inpho/inpho.ini")]
 
@@ -10,6 +11,8 @@ for path in config_paths:
     if os.path.exists(path):
         config_path = path
         break
+
+logging.debug("Using config path: %s" % config_path)
 
 # Configuration file does not exist, raise Exception
 if config_path is None:
