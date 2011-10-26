eea290e838903f0651ccead28aa549e483f15ce7
Jaimie Murdock <jaimie.murdock@gmail.com>
2011-10-26 11:30:59 -0400

    updated documentation

--- inpho/dlv/__init__.py
+++ inpho/dlv/__init__.py
@@ -1,16 +1,27 @@
 from inpho.model import *
 
 def generate_input():
+    """
+    Prints the DLV input to STDOUT
+    """
+    # Set of terms which appear
     appearances = set()
     appearances.update(print_taxonomy())
     appearances.update(print_evaluations(IdeaEvaluation))
     appearances.update(print_evaluations(AnonIdeaEvaluation))
     print_appearances(appearances)
 
-# Set of terms which appear
 def print_taxonomy():
+    """
+    Prints DLV input for the taxonomy, covering the class and isa atoms.
+    Returns the list of terms which occur in the database.
+    """
+    # Set of terms which appear
     appearances = set()
+    
+    # Select concept_id and parent_concept_id pairs
     ids = Session.query(Node.concept_id, Node.parent_concept_id).all()
+
     # Build up atoms for hand-built taxonomy
     for concept_id, parent_concept_id in ids:
         appearances.update([concept_id, parent_concept_id])
@@ -21,8 +32,11 @@ def print_taxonomy():
 
     return appearances
 
-# Build up atoms for evaluations
 def print_evaluations(type=IdeaEvaluation):
+    """
+    Prints DLV atoms for evaluations, covering the p#, mg, ms, sg, and ic.
+    Returns the list of terms which occur in the database.
+    """
     appearances = set()
 
     evals = Session.query(type).limit(10).all()
@@ -49,10 +63,15 @@ def print_evaluations(type=IdeaEvaluation):
 
     return appearances
 
-# Build up atoms for terms that appear
 def print_appearances(appearances):
+    """
+    Prints DLV atoms for appearances, covering the i atom.
+    Returns the same as the input variable.
+    """
     for idea in appearances:
         print "i(i%s)." % idea
+
+    return appearances
 
 if __name__ == '__main__':
     generate_input()
