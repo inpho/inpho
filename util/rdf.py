42c534e90da69e790bcda765edc0814412c06f81
Wesley Pettyjohn <wapettyj@indiana.edu>
2011-10-28 01:43:39 -0400

    added Users, Ideas, and ConceptSchemes

--- util/rdf.py
+++ util/rdf.py
@@ -16,6 +16,10 @@ g.bind("inpho", "http://inpho.cogs.indiana.edu/")
 t = Namespace("http://inpho.cogs.indiana.edu/thinker/")
 g.bind("thinker", "http://inpho.cogs.indiana.edu/thinker/")
 
+# user namespace currently doesn't exist?
+u = Namespace("http://inpho.cogs.indiana.edu/user/")
+g.bind("user", "http://inpho.cogs.indiana.edu/user/")
+
 foaf = Namespace("http://xmlns.com/foaf/0.1/")
 g.bind("foaf", "http://xmlns.com/foaf/0.1/")
 
@@ -27,6 +31,9 @@ g.bind("rdfs", "http://www.w3.org/TR/rdf-schema/#")
 
 owl = Namespace("http://www.w3.org/2002/07/owl#")
 g.bind("owl", "http://www.w3.org/2002/07/owl#")
+
+i = Namespace("http://inpho.cogs.indiana.edu/idea")
+g.bind("idea", "http://inpho.cogs.indiana.edu/idea")
 
 skos = Namespace("http://www.w3.org/2004/02/skos/core#")
 g.bind("skos", "http://www.w3.org/2004/02/skos/core#")
@@ -41,6 +48,31 @@ g.add((inpho['thinker'], rdfs['subClassOf'], inpho['entity']))
 for thinker in thinkers:
     g.add((t['t' + str(thinker.ID)], rdf['type'], inpho['thinker']))
     g.add((t['t' + str(thinker.ID)], foaf['name'], Literal(thinker.label)))
+
+# Select all ConceptSchemes
+conceptschemes = Session.query(ConceptSchemes).all()
+g.add((skos['conceptscheme'], skos['hasTopConcept'], inpho['idea']))
+g.add((inpho['idea'], rfds['subClassOf'], inpho['entity']))
+for conceptscheme in conceptschemes:
+    g.add((skos['s' + str(conceptscheme.ID)], inpho['taxonomy'], inpho['includes']))
+
+#Select all Ideas
+ideas = Session.query{Ideas).all()
+g.add((inpho['idea'], rdfs['subClassOf'], inpho['entity']))
+g.add((inpho['idea'], rdfs['subClassOf'], skos['Concept'])) 
+for idea in ideas:
+    g.add((i['idea' + string(idea.ID)], rdf['type'], inpho['idea']))
+
+# Never create an instance of an inpho:user, used to tag provenance of evaluations
+# Select all Users
+users = Session.query(Users).all()
+g.add((inpho['user'], rdf['type'], foaf['Person']))
+g.add((inpho['expert_user'], rdfs['subClassOf'], inpho['user']))
+g.add((inpho['turk_user'], rdf['type'], foaf['Person']))
+g.add((inpho['admin'], rdfs['subClassOf'], inpho['user']))
+for user in users:
+    g.add((u['u' + str(user.ID)], rdf['type'], inpho['user']))
+
 
 with open("out.rdf", "w") as f:
     f.write(g.serialize(format="n3"))
