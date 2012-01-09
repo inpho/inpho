fb20f484137eb2b99a4cbfefaa16ae4a19e20880
Jaimie Murdock <jaimie.murdock@gmail.com>
2012-01-09 16:46:46 -0500

    added Date constructor

--- inpho/model/date.py
+++ inpho/model/date.py
@@ -1,2 +1,10 @@
 class Date(object):
-    pass
+    """ SQLAlchemy wrapper for Date objects """
+
+    def __init__(self, entity_id, relation_id, year, month=None, day=None):
+        self.entity_id = entity_id
+        self.relation_id = relation_id
+        self.year = year
+        self.month = month
+        self.day = day
+
