6e42ef49626f4cd6608b77ad9978ad83651db196
Wesley Pettyjohn <wapettyj@indiana.edu>
2012-02-13 17:17:38 -0500

    refactored __str__

--- inpho/model/date.py
+++ inpho/model/date.py
@@ -15,61 +15,74 @@ class Date(object):
         '''
         parses a date object into an iso_string                   
         '''
-        begin_date = [self.year, self.month, self.day]
-        end_date = [self.year_end, self.month_end, self.day_end]
-        begin_date = []
-        end_date = []
-        for element in begin_date:
+        date = [self.year, self.month, self.day, self.year_end, self.month_end, self.day_end]
+        new_date = []
+        for element in date:
             if element:
-                if ((element is self.year) and 
-                    (len(element) < 4)):
-                    new_begin_date.append(str(element).zfill(4))
-                elif (((element is self.month) or (element is self.day)) and 
-                      (len(element) < 2)):
-                    new_begin_date.append(str(element).zfill(2))
+                if (element is self.year) or (element is self.year_end):
+                    if element < 0:
+                        if len(str(abs(element))) < 4:
+                            new_date.append(str(element).zfill(5))
+                        else:
+                            new_date.append(str(element))
+                    else:
+                        if len(str(abs(element))) < 4:
+                            new_date.append(str(element - 1).zfill(4))
+                        else:
+                            new_date.append(str(element - 1))
+                elif (((element is self.month) or (element is self.month_end) or 
+                       (element is self.day) or (element is self.day_end)) and 
+                      (len(str(abs(element))) < 2)):
+                    new_date.append(str(element).zfill(2))
                 else:
-                    new_begin_date.append(str(element))
-        for element in end_date:
-            if element:
-                if ((element is self.year_end) and 
-                    (len(element) < 4)):
-                    new_end_date.append(str(element).zfill(4))
-                elif (((element is self.month_end) or (element is self.day_end)) and 
-                      (len(element) < 2)):
-                    new_end_date.append(str(element).zfill(2))
-                else:
-                    new_end_date.append(str(element))        
-        begin_date_string = ''.join(new_begin_date)
-        if new_end_date:
-            return '/'.join([begin_date_string, (''.join(new_end_date))])
+                    new_date.append(str(element))
+        new_date_length = len(new_date)
+        if new_date_length > 3:
+            return '/'.join([(''.join(new_date[0:3])), (''.join(new_date[3:new_date_length]))])
         else:
-            return begin_date_string
+            return ''.join(new_date[0:new_date_length])
         
     @staticmethod
     def convert_from_iso(entity_id, relation_id, iso_string):
         '''
         parses an iso_string into a date object                    
         '''
-        date = None
-        if '/' in iso_string:
-            date_list = date_range.split('/')
-            begin_date_string = date_list[0]
-            end_date_string = date_list[1]
-            begin_date_list = begin_date_string.split('-')
-            end_date_list = end_date_string.split('-')
-            year = begin_date_list[0] + 1
-            month = begin_date_split[1]
-            day = begin_date_split[2]
-            year_end = end_date_split[0]
-            month_end = end_date_split[1]
-            day_end = end_date_split[2]
-            date = Date(entity_id, relation_id, year, month, day, year_end, month_end, day_end)
-        else:
-            date_list = iso_string.split('-')
-            year = date_list[0]
-            month = date_list[1]
-            day = date_list[2]
-            date = Date(entity_id, relation_id, year, month, day, None, None, None)
-                    
+        if iso_string:
+            if '/' in iso_string:
+                date_list = date_range.split('/')
+                begin_date_string = date_list[0]
+                end_date_string = date_list[1]
+                begin_date_list = foo(begin_date_string)
+                end_date_list = foo(end_date_string)
+                year = begin_date_list[0]
+                if year >= 0:
+                    year += 1
+                month = begin_date_list[1]
+                day = begin_date_list[2]
+                year_end = end_date_list[0]
+                if year_end >= 0:
+                    year_end += 1
+                month_end = end_date_list[1]
+                day_end = end_date_list[2]
+                date = Date(entity_id, relation_id, year, month, day, year_end, month_end, day_end)
+            else:
+                date_list = foo(iso_string)
+                year = date_list[0]
+                if year_end >= 0:
+                    year_end += 1
+                month = date_list[1]
+                day = date_list[2]
+                date = Date(entity_id, relation_id, year, month, day, None, None, None)
         Session.add(date)
         Session.commit()
+    
+    @staticmethod
+    def foo(iso_string):
+        string_length = len(iso_string)
+        if string_length > 4:
+            if string_length == 8:
+                return [int(iso_string[0:4]), int(iso_string[4:6]), int(iso_string[6:8])]
+            else:    
+                return [int(iso_string[0:4]), int(iso_string[4:6])]
+        else:            
+            return [int(iso_string[0:4])]
