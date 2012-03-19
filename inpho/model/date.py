0a16adda192be9357b2e56b7ff2d3d1b273218fd
Jaimie Murdock <jaimie.murdock@gmail.com>
2012-03-19 16:18:16 -0400

    fixed convert_from_iso: no longer commits to session

--- inpho/model/date.py
+++ inpho/model/date.py
@@ -1,5 +1,3 @@
-from inpho.model import *
-
 month_dict = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May',
               6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 
               11:'November', 12:'December'}
@@ -125,5 +123,5 @@ class Date(object):
                 month = date_list[1]
                 day = date_list[2]
                 date = Date(entity_id, relation_id, year, month, day, None, None, None)
-        Session.add(date)
-        Session.commit()
+
+        return date
