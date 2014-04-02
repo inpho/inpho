c366e6245645677b0431fb1290590b5b92366fc6
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-04-02 16:50:14 -0400

    fixing html, sorting list

--- util/septaxonomy.py
+++ util/septaxonomy.py
@@ -2,42 +2,34 @@ from inpho.model import Session, Node, Idea
 
 def print_sep(node, depth=0):
     print "<li>"
-    sep_dir = node.sep_dir if isinstance(node, Idea) else node.idea.sep_dir
 
+    # Print SEP Link
+    sep_dir = node.sep_dir if isinstance(node, Idea) else node.idea.sep_dir
     if sep_dir:
         print '<a href="http://plato.stanford.edu/entries/%s">%s</a>' % (sep_dir, node.label)
     else:
         print node.label
 
-    print "".join(["    "*depth, node.label]),
+    # Print descendants
+    descendants = list()
     if isinstance(node, Node):
-        if node.idea.sep_dir:
-            print "".join([" +", node.idea.sep_dir])
-        else:
-            print " "
-        if node.children or node.idea.instances:
-            print "<ul>"
-            for idea in node.children:
-                print_sep(idea, depth+1)
-            for idea in node.idea.instances:
-                print_sep(idea, depth+1)
-            print "</ul>"
+        descendants.extend(node.children)
+        descendants.extend(node.idea.instances)
     else:
-        if node.sep_dir:
-            print "".join([" +", node.sep_dir])
-        else:
-            print " "
+        descendants.extend(node.instances)
 
-        if node.instances:
-            print "<ul>"
-            for idea in node.instances:
-                print_sep(idea, depth+1)
-            print "</ul>"
+
+    if descendants:
+        print "<ul>"
+        for idea in sorted(descendants, key=lambda x: x.label):
+            print_sep(idea, depth+1)
+        print "</ul>"
+    
     print "</li>"
 
 roots = Session.query(Node).filter(Node.parent_id==None).all()
 print "<ul>"
-for root in roots:
+for root in sorted(roots, key=lambda x: x.label):
     print_sep(root)
 print "</ul>"
 
